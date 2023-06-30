7d9afbeabcbc215f0ee5fe48f4ebc6724038d79c
KayBahr <124631379+KayBahr@users.noreply.github.com>
2023-06-30 22:47:51 +0000

    routes base commit

--- src/front/js/pages/bottom10.js
+++ src/front/js/pages/bottom10.js
@@ -4,7 +4,7 @@ export const Bottom10 = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Bottom 10</h1>
+			<h1>Bottom 10!</h1>
 		</div>
 	);
 };
--- src/front/js/pages/top10.js
+++ src/front/js/pages/top10.js
@@ -4,7 +4,7 @@ export const Top10 = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Top 10</h1>
+			<h1>Top 10!</h1>
 		</div>
 	);
 };

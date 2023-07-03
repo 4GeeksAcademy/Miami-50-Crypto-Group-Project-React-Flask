7e4bf2a176c78b113b09dcf2482872ff54798b70
Tara <tmarieshelnutt@gmail.com>
2023-07-03 20:48:51 +0000

    More work on the feed...

--- src/front/js/component/NewsFeed.js
+++ src/front/js/component/NewsFeed.js
@@ -2,60 +2,59 @@ import { useEffect, useState } from 'react';
 
 import React from 'react';
 import "../../styles/newsfeed.css";
+import axios from 'axios';
+
 
 
 
 const NewsFeed = () => {
-  const [news, setNews] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
+    const [news, setNews] = useState('');
+    const [currentIndex, setCurrentIndex] = useState(0);
+  
+    useEffect(() => {
+      const axios = require('axios');
 
-  useEffect(() => {
-    
-    const fetchNews = async () => {
-      try {
-        const response = await fetch('https://crypto-news16.p.rapidapi.com/news/cryptoninjas');
-        const data = await response.json();
-        
-        setNews(data);
-      } catch (error) {
-        console.error('Error fetching news:', error);
-      }
-    };
+      const options = {
+        method: 'GET',
+        url: 'https://crypto-news-live9.p.rapidapi.com/news/CryptoNews',
+        headers: {
+          'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+          'X-RapidAPI-Host': 'crypto-news-live9.p.rapidapi.com'
+  }
+}
 
-    fetchNews();
-  }, []);
+    axios.request(options).then((response) => {
+      console.log(response.data)
+      setNews(response.data)
+    }).catch((error) => {
+      console.error(error)
+    });
+    }, [])
+      console.log(news)
 
-  useEffect(() => {
-    // Rotate news every 10 seconds
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) => (prevIndex + 1) % news.length);
-    }, 10000);
-
-    return () => clearInterval(interval);
-  }, [news]);
-
-  return (
-    <div className="news-feed">
-      <h2>Cryptocurrency News</h2>
-      {news.length > 10 ? (
-        <div className="carousel">
-          {news.map((item, index) => (
-            <div
-              key={index}
-              className={`carousel-item ${index === currentIndex ? 'active' : ''}`}
-            >
-              <h3>{item.title}</h3>
-              <p>{item.content}</p>
-              <p>Source: {item.source}</p>
-            </div>
-          ))}
-        </div>
-      ) : (
-        <p>Loading news...</p>
-      )}
-    </div>
-  );
-};
+   
+    return (
+      <div className="news-feed">
+        <h2>Cryptocurrency News</h2>
+        {news.length > 0 ? (
+          <div className="carousel">
+            {news.map((item, index) => (
+              <div
+                key={index}
+                className={`carousel-item ${index === currentIndex ? 'active' : ''}`}
+              >
+                <h3>{item.title}</h3>
+                <p>{item.summary}</p>
+                <p>Source: {item.source}</p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>Loading news...</p>
+        )}
+      </div>
+    );
+  };
 
 export default NewsFeed;
 

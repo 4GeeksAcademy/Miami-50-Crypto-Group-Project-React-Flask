1043a70f997c90e0f5f57a03cd5f61f59a24d9cc
KayBahr <124631379+KayBahr@users.noreply.github.com>
2023-07-08 20:29:26 +0000

    favorites fav.map commit

--- src/front/js/pages/favorites.js
+++ src/front/js/pages/favorites.js
@@ -1,56 +1,28 @@
 import React from "react";
 import "../../styles/favorite.css";
 
-export const Favorites = () => {
+export const Favorites = ({ fav }) => {
 
 	return (
 		<div className="text-center mt-5">
 			<h1>Favorites</h1>
 			<div className="d-flex justify-content-center">
-				<div className="container text-center">
-					<div className="row">
-						<div className="col">
-							<div className="card">
-								<div className="card-body">
-								<h5 className="card-title">Name</h5>
-									<p className="card-text">--^-v</p>
-									<p className="card-text">%</p>
-									<p className="card-text">$</p>
-								</div>
-							</div>
+			  <div className="container text-center">
+				<div className="row">
+				  {Array.isArray(fav) && fav.map((favItem, index) => (
+					<div className="col" key={index}>
+					  <div className="card">
+						<div className="card-body">
+						  <h5 className="card-title">{asset.icon}{favItem}</h5>
+						  <p className="card-text">{asset.chart}</p>
+						  <p className="card-text">{asset.change}</p>
+						  <p className="card-text">{asset.amount}</p>
 						</div>
-						<div className="col">
-							<div className="card">
-								<div className="card-body">
-								<h5 className="card-title">Name</h5>
-									<p className="card-text">--^-v</p>
-									<p className="card-text">%</p>
-									<p className="card-text">$</p>
-								</div>
-							</div>
-						</div>
-						<div className="col">
-							<div className="card">
-								<div className="card-body">
-									<h5 className="card-title">Name</h5>
-									<p className="card-text">--^-v</p>
-									<p className="card-text">%</p>
-									<p className="card-text">$</p>
-								</div>
-							</div>
-						</div>
-						<div className="col">
-							<div className="card">
-								<div className="card-body">
-									<h5 className="card-title">Name</h5>
-									<p className="card-text">--^-v</p>
-									<p className="card-text">%</p>
-									<p className="card-text">$</p>
-								</div>
-							</div>
-						</div>
+					  </div>
 					</div>
+				  ))}
 				</div>
+			  </div>
 			</div>
 		</div>
 	);

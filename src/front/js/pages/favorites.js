77ee6df5e4d311f975981dceae880a9f06b39c8b
KayBahr <124631379+KayBahr@users.noreply.github.com>
2023-07-12 23:02:33 +0000

    topbottomfav front end structure commit

--- src/front/js/pages/favorites.js
+++ src/front/js/pages/favorites.js
@@ -7,16 +7,16 @@ export const Favorites = ({ fav }) => {
 		<div className="text-center mt-5">
 			<h1>Favorites</h1>
 			<div className="d-flex justify-content-center">
-			  <div className="container text-center">
+			  <div className="container">
 				<div className="row">
 				  {Array.isArray(fav) && fav.map((favItem, index) => (
 					<div className="col" key={index}>
 					  <div className="card">
 						<div className="card-body">
-						  <h5 className="card-title">{asset.icon}{favItem}</h5>
-						  <p className="card-text">{asset.chart}</p>
-						  <p className="card-text">{asset.change}</p>
-						  <p className="card-text">{asset.amount}</p>
+						  <h5 className="card-title">Name{asset.icon}{favItem}</h5>
+						  <div className="card-text" id="chart">Chart{asset.chart}</div>
+						  <p className="card-text" id="percent">Change{asset.change}</p>
+						  <h6 className="card-text" id="amount">Amount{asset.amount}</h6>
 						</div>
 					  </div>
 					</div>
--- src/front/js/pages/top10.js
+++ src/front/js/pages/top10.js
@@ -11,81 +11,81 @@ export const Top10 = () => {
 					<tr>
 					<th scope="col">Name</th>
 					<th scope="col">Price</th>
+					<th scope="col">Chart</th>
 					<th scope="col">Change</th>
 					<th scope="col">Market Cap</th>
-					<th scope="col">Action</th>
 					</tr>
 				</thead>
 				<tbody>
 					<tr>
 					<th scope="row">First</th>
 					<td>$</td>
+					<td>--^-v-</td>
 					<td>%</td>
 					<td>$</td>
-					<td><button>Buy/Sell</button></td>
 					</tr>
 					<tr>
 					<th scope="row">Second</th>
 					<td>$</td>
+					<td>--^-v-</td>
 					<td>%</td>
 					<td>$</td>
-					<td><button>Buy/Sell</button></td>
 					</tr>
 					<tr>
 					<th scope="row">Third</th>
 					<td>$</td>
+					<td>--^-v-</td>
 					<td>%</td>
 					<td>$</td>
-					<td><button>Buy/Sell</button></td>
 					</tr>
 					<tr>
 					<th scope="row">Fourth</th>
 					<td>$</td>
+					<td>--^-v-</td>
 					<td>%</td>
 					<td>$</td>
-					<td><button>Buy/Sell</button></td>
 					</tr>
 					<tr>
 					<th scope="row">Fifth</th>
 					<td>$</td>
+					<td>--^-v-</td>
 					<td>%</td>
 					<td>$</td>
-					<td><button>Buy/Sell</button></td>
 					</tr>
 					<tr>
 					<th scope="row">Sixth</th>
 					<td>$</td>
+					<td>--^-v-</td>
 					<td>%</td>
 					<td>$</td>
-					<td><button>Buy/Sell</button></td>
 					</tr>
 					<tr>
 					<th scope="row">Seventh</th>
 					<td>$</td>
+					<td>--^-v-</td>
 					<td>%</td>
 					<td>$</td>
-					<td><button>Buy/Sell</button></td>
 					</tr>
 					<tr>
 					<th scope="row">Eighth</th>
 					<td>$</td>
+					<td>--^-v-</td>
 					<td>%</td>
 					<td>$</td>
-					<td><button>Buy/Sell</button></td>
 					</tr>
 					<tr>
 					<th scope="row">Ninth</th>
 					<td>$</td>
+					<td>--^-v-</td>
 					<td>%</td>
 					<td>$</td>
-					<td><button>Buy/Sell</button></td>
 					</tr>
 					<tr>
 					<th scope="row">Tenth</th>
 					<td>$</td>
+					<td>--^-v-</td>
 					<td>%</td>
 					<td>$</td>
-					<td><button>Buy/Sell</button></td>
 					</tr>
 				</tbody>
 			</table>
